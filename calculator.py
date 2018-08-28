93c6f3ead08f38bfde3a470be87aabdd868bde3e
rsliter <rsliter@etsy.com>
2018-08-28 16:44:12 -0400

    Adding a subtract function to the calculator

--- calculator.py
+++ calculator.py
@@ -2,5 +2,11 @@ class Calculator:
     def add(self, x, y):
         return x + y
 
+    def subtract(self, x, y):
+        return x - y
+
 calculator = Calculator()
+print("1 + 2 = ....")
 print(calculator.add(1,2))
+print("1 - 2 = ....")
+print(calculator.subtract(1,2))
